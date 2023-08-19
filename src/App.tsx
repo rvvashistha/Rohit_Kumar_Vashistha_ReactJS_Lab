1a3c47c6fc716bdc42c675370ec082011f871b67
rvvashistha <rohitvashistha171@gmail.com>
2023-08-19 23:40:50 +0530

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import ExpenseTracker from './Components/ExpenseTracker';
+import ShowData from './Components/ShowList';
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/add" component={ExpenseTracker} />
+          <Route path="/" component={ShowData} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+function Test () {
+
+    const sta = ["dfv","sfdv","rtg"]
+    return (
+        <>
+            {
+                sta && (
+                    sta.map(
+                        ab => (
+                            <div>{ab}</div>
+                        )
+                    )
+                )
+                
+            }
+        </>
+    )
+}
+export default Test
